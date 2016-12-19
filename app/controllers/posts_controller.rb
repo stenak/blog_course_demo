643351a3402133f43b54ad07af9581ea5de0cac1
Steve Nakevski <s.nakevski@live.com>
2016-12-19 22:47:47 +0100

    CRUD creation started

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,45 @@
+class PostsController < ApplicationController
+	
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+
+	def index
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new post_params
+
+		if @post.save
+			redirect_to @post, notice: "Amazing, Steve! Your post was successfully saved!"
+		else
+			render 'new', notice: "Oh no, I was unable to save your post!"
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @post.update post_params
+			redirect_to @post, notice: "Great! Your post was successfully saved!"
+		else
+			render 'edit'
+		end
+	end
+
+	private 
+
+	def post_params
+	params.require(:post).permit(:title, :content)
+	end
+
+	def find_post
+		@post = Post.find(params[:id])
+	end
+end
